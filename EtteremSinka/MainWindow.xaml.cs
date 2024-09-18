30c6f85cf19c48e4fff2afeba42f3cef960db813
vedran.krisztian <vedran.krisztian@BURG-1E06-13>
2024-09-18 14:34:11 +0200

    szia nyaloka

---
+++ EtteremSinka/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EtteremSinka
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Switch to Registration form
+        private void SwitchToRegister(object sender, RoutedEventArgs e)
+        {
+            LoginPanel.Visibility = Visibility.Collapsed;
+            RegistrationPanel.Visibility = Visibility.Visible;
+        }
+
+        // Switch to Login form
+        private void SwitchToLogin(object sender, RoutedEventArgs e)
+        {
+            RegistrationPanel.Visibility = Visibility.Collapsed;
+            LoginPanel.Visibility = Visibility.Visible;
+        }
+    }
+}
